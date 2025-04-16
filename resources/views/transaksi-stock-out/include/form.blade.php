34e6d6126e351ab59a73f3384f2cece4cbfe82ec
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2025-04-16 18:33:40 +0700

    update

--- resources/views/transaksi-stock-out/include/form.blade.php
+++ resources/views/transaksi-stock-out/include/form.blade.php
@@ -165,9 +165,6 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title">Tambah Barang</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
             </div>
             <div class="modal-body table-responsive">
                 <div class="container-fluid">
